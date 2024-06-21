d9fbbd69c90d85b8dcb88f09e01665b8b3770713
chenwi32 <chenwieugene.j@gmail.com>
2024-06-21 08:13:28 +0100

    create a schema with user and podcast table

---
+++ convex/users.ts
@@ -0,0 +1,10 @@
+import { internalMutation } from "./_generated/server";
+
+export const createUser = internalMutation({
+    args: {
+        
+    },
+    handler(ctx, args) {
+        
+    },
+})
