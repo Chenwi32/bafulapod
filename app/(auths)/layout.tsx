548df46fe4042feab1d0cf8b1cd72cb325baab2d
chenwi32 <chenwieugene.j@gmail.com>
2024-06-27 07:13:59 +0100

    create sing up and sign in page and started configurations

--- app/(auths)/layout.tsx
+++ app/(auths)/layout.tsx
@@ -1,13 +1,23 @@
-import { ReadonlyURLSearchParams } from "next/navigation";
+import { Metadata } from "next";
+import Image from "next/image";
 
-export default function AuthLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-    return (
-        <html>
-            <body>
-                <main>
-                    {children}
-                </main>
-            </body>
-        </html>
-    )
+export const metadata: Metadata = {
+  title: "BafulaPod",
+  description: "Bafut Language Podcast Platform",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className=" relative h-screen w-full">
+      <div className="bg-slate-600 absolute w-full">
+        <Image src={"/icons/logo1.svg"} alt="" fill className="size-full" />
+      </div>
+      {children}
+    </main>
+  );
 }
