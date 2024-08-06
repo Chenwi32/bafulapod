eefefbc6940c2cf3cee1bad7eb28625c86fbded1
chenwi32 <chenwieugene.j@gmail.com>
2024-08-06 04:39:24 +0100

    Fixed the blury thumbnail issue

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { ImageDown, ImageIcon } from "lucide-react";
 
 const PodcastCard = ({
   id,
@@ -19,28 +22,36 @@ const PodcastCard = ({
  /*  console.log(imageUrl) */
   return (
     <div className="bg-slate-200 shadow-lg  w-full rounded">
-      <figure className="flex w-full p-0 justify-center flex-col gap-2">
-      
-      
-          <Image
+      <figure className="flex w-full max-w-max p-0 justify-center flex-col gap-2">
+        {
+          imageUrl === undefined ? (
+            <>
+              <ImageIcon className="size-10" />
+              Thumbnail not found
+            </>
+          ): (
+              <>
+              <Image
           src={imageUrl}
-        
           alt={title}
-          width={45}
-          height={50}
-            className="aspect-square h-fit w-full rounded-xl"
-        /> 
-        
+          width={500}
+          height={250}
+          className="aspect-auto h-fit w-full rounded-xl"
+        />
+          </>
+          
+          )
+     }   
+
         <div className="p-5 w-full">
-        <audio className="mb-5" src={audioUrl} controls />
+          <audio className="w-full mb-5" src={audioUrl} controls />
 
-        <h1 className="text-xl font-bold truncate">{title}</h1>
-        <h1 className="  truncate">
-          Author: <span className="font-bold">{author} </span>
-        </h1>
-        <h1 className="text-12 capitalize truncate">{description}</h1>
+          <h1 className="text-xl font-bold truncate">{title}</h1>
+          <h1 className="  truncate">
+            Author: <span className="font-bold">{author} </span>
+          </h1>
+          <h1 className="text-12 capitalize truncate">{description}</h1>
         </div>
-
       </figure>
     </div>
   );
