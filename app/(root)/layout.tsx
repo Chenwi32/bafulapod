b7d7a1b21e1b8b619457e769bf6c92cf3ade0d36
ProfilA237 <profila237@gmail.com>
2024-08-12 08:33:22 +0100

    fixed top space

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -38,33 +38,29 @@ export default function Layout({
       </Head>
       <ToastProvider>
         <body className="relative ">
-          
-            <div className=" bg-white z-10 py-2 px-5 shadow-md fixed w-full mb-5">
-              <Navbar />
-            </div>
-
-            <div className=" container p-0 sm:p-5 flex top-16 justify-between">
-              <div className="hidden lg:block">
-                <LeftSideBar />
-              </div>
-
-              <section className=" flex flex-1 min-h-screen flex-col  lg:px-1 ">
-                <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
-                  <div className="flex flex-col  md:pb-14">
-                    <Toaster />
-
-                    {children}
-                  </div>
-                </div>
-              </section>
-
-             
-            
+          <div className=" bg-white z-10 top-0 py-2 px-5 shadow-md fixed w-full mb-5">
+            <Navbar />
           </div>
 
-           <div className="brand-1">
-                <Footer/>
-          </div> 
+          <div className=" container p-0 sm:p-5 flex mt-20 justify-between">
+            <div className="hidden lg:block">
+              <LeftSideBar />
+            </div>
+
+            <section className=" flex flex-1 min-h-screen flex-col  lg:px-1 ">
+              <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+                <div className="flex flex-col  md:pb-14">
+                  <Toaster />
+
+                  {children}
+                </div>
+              </div>
+            </section>
+          </div>
+
+          <div className="brand-1">
+            <Footer />
+          </div>
         </body>
       </ToastProvider>
     </html>
