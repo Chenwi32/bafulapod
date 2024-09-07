727ef75a0448fc256ffa61c9e76d4dbbf839a7a2
chenwi32 <chenwieugene.j@gmail.com>
2024-09-07 07:11:06 +0100

    Reactivatetd Trendings

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,8 +25,8 @@ const page = () => {
           <Banner />
         </div>
 
-        {/* This displays trending podcast on the root homepage and is disabled now pending convex proaccout activation
-        <TrendingPodcast /> */}
+        {/* This displays trending podcast on the root homepage and is disabled now pending convex proaccout activation*/}
+        <TrendingPodcast /> 
       </div>
     </>
   );
